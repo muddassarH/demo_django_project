fbde48d2dfdbd78553b2499a7da56f9e745777fe
muddassarH <muddassarhussain90@gmail.com>
2025-05-24 02:36:12 +0500

    changes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
+# api/models.py
 from django.db import models
 
-# Create your models here.
+class Greeting(models.Model):
+    message = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.message
